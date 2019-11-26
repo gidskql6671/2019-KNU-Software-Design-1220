120dbf65ce7ccf3c0e707502a33ddf568a981478
Hyunseung Jeon <dogdriip@gmail.com>
2019-11-26 20:06:27 +0900

    add login_test.py, register_test.py

--- db_test.py
+++ db_test.py
@@ -3,7 +3,20 @@ import sqlite3
 conn = sqlite3.connect('db/student.db')
 
 cur = conn.cursor()
-cur.execute("insert into student values('test', 'test', '2018123123', '홍길동', '컴퓨터학부', 'Y')")
+
+### insert
+# cur.execute("insert into student values('test', 'test', '2018123123', '홍길동', '컴퓨터학부', 'Y')")
+
+### fetch
+user_id = 'test'
+user_password = 'test'
+cur.execute('SELECT * FROM student WHERE id=? AND password=?', (user_id, user_password))
+data = cur.fetchone()
+print(type(data))
+if cur.fetchone() is not None:
+    print("Welcome")
+else:
+    print("Login failed")
 
 conn.commit()
 
