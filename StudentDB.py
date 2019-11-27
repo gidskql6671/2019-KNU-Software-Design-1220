f0e3e9726cea5f5d5e93ea8b257434e7e28f2f07
Hyunseung Jeon <dogdriip@gmail.com>
2019-11-27 19:58:07 +0900

    DB 계정 정보 수정 method 작성중

--- StudentDB.py
+++ StudentDB.py
@@ -22,6 +22,12 @@ class StudentDB(object):
         data = self.cur.fetchone()
         self.conn.commit()
         return data
+
+    # args로 (user_id, user_pw, student_id, name, major, is_abeek) 6개 항목 tuple을 주면, user_id, user_pw로 검색한 행을 넘겨준 전체 정보로 갱신
+    # TODO: 여러 개 한 번에 update하는 것보다 (수정할 필드 이름, 수정할 정보)로 받는 게 낫겠다
+    def update(self, args: tuple):
+        self.cur.execute('UPDATE student SET  WHERE id=? AND password=?', args)
+        self.conn.commit()
     
     # args로 (user_id, user_pw) tuple을 주면, 해당 행을 삭제
     def delete(self, args: tuple):
