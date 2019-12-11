8939cf1e03f9c4cae72de23161df3a4a57ca5dd2
Hyunseung Jeon <dogdriip@gmail.com>
2019-12-12 01:55:09 +0900

    학생 계정정보 수정 구현

--- StudentDB.py
+++ StudentDB.py
@@ -11,11 +11,13 @@ class StudentDB(object):
         self.cur = self.conn.cursor()
         
 
+    # 계정 생성
     # args로 (user_id, user_pw, student_id, name, major, is_abeek) 6개 항목 tuple을 줘야 함
     def register(self, args: tuple):
         self.cur.execute('INSERT INTO student VALUES(?, ?, ?, ?, ?, ?)', args)
         self.conn.commit()
 
+    # 계정 정보 검색 (로그인)
     # args로 (user_id, user_pw) tuple을 주면, 그 row의 정보 6개를 tuple로 리턴 (결과가 없으면 None을 리턴)
     def search(self, args: tuple):
         self.cur.execute('SELECT * FROM student WHERE id=? AND password=?', args)
@@ -23,12 +25,14 @@ class StudentDB(object):
         self.conn.commit()
         return data
 
+    # 계정 정보 수정
     # args로 (user_id, user_pw, student_id, name, major, is_abeek) 6개 항목 tuple을 주면, user_id, user_pw로 검색한 행을 넘겨준 전체 정보로 갱신
-    # TODO: 여러 개 한 번에 update하는 것보다 (수정할 필드 이름, 수정할 정보)로 받는 게 낫겠다
     def update(self, args: tuple):
-        self.cur.execute('UPDATE student SET  WHERE id=? AND password=?', args)
+        (user_id, user_pw, student_id, name, major, is_abeek) = args
+        self.cur.execute(f'UPDATE student SET id={user_id}, password={user_pw}, student_id={student_id}, name={name}, major={major}, is_abeek={is_abeek} WHERE id=? AND password=?', args)
         self.conn.commit()
     
+    # 계정 탈퇴 (계정 정보만 삭제)
     # args로 (user_id, user_pw) tuple을 주면, 해당 행을 삭제
     def delete(self, args: tuple):
         self.cur.execute('DELETE * FROM student WHERE id=? AND password=?', args)
