3ed0c4a13ccbe2af5fa27a8086f3486aff33b24b
joonhey shin <jhshin0028@gmail.com>
2019-12-13 22:04:33 +0900

    졸업요건클래스,심컴클래스(12 이전, 이후) 추가, 패키지 정리

---
+++ GraduationRequirements/GradRequirement.py
@@ -0,0 +1,17 @@
+class GradRequirement:
+
+    def __init__(self, total_credits, if_docsubmit,majorname):
+        self.total_credits = total_credits
+        self.if_docsubmit = if_docsubmit
+        # self.majorname = majorname
+
+    #디비에 해당 학과의 졸업 요건 추가
+    def add_requirement(self):
+        pass
+    #디비에 해당 학과의 졸업요건 변경
+    def update_requirment(self):
+        pass
+    #디비에 해당 학과의 졸업요건 삭제
+    def del_requirment(self):
+        pass
+
