6aa11c15d6fd2309847e16b8fc23dc4f6a4fbac3
KHyoseon <58212404+KHyoseon@users.noreply.github.com>
2019-12-16 19:37:40 +0900

    교직원 클래스 수정

--- UserAccount/Faculty.py
+++ UserAccount/Faculty.py
@@ -4,27 +4,23 @@ from UserAccount import UserAccount
 
 class Faculty(UserAccount):
 
-    def __init__(self, user_id, user_pw, faculty_id, name, department):
-        self._user_id = user_id
-        self._user_pw = user_pw
-        self._faculty_id = ""
-        self._name = ""
-        self._department = ""
+    def __init__(self):
+        self._user_DB = FacultyDB()
+        self._user_info = None
 
-    def get_info(self, user_id, user_pw, faculty_id, name, department):
-        self._user_id = user_id
-        self._user_pw = user_pw
-        self._faculty_id = faculty_id
-        self._name = name
-        self._department = department
+    def get_info(self):
+        return self._user_info
 
-    def login( self, user_id, user_pw ):
-        user_DB = FacultyDB()
-        # account exists => return True
-        if user_DB.search((user_id, user_pw)):
-            return True
-        # account doesn't exist => return True
-        # login() == F 일 경우 self.get_info( , , , , , )로 전체 사용자 정보 받아오고 register로 등록
-        # user_DB.register((self._user_id, self._user_pw, self._faculty_id, self._name, self._department))
-        else:
+    def login(self, input_id, input_pwd):
+
+        self._user_info = self._user_DB.search((input_id, input_pwd))
+        if None:
             return False
+        else:
+            return True
+
+    def register_acc(self, user_id, user_pw, Faculty_id, name, department):
+        self._user_DB.register((user_id, user_pw, Faculty_id, name, department))
+
+    def del_acc(self, user_id, user_pw):
+        self._user_DB.delete((self._user_id, self._user_pw))
