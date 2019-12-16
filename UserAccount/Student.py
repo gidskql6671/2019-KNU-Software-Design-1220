3f90cd6983c1d1a56d30de1f45d5e6452a49409a
gidskql6671 <gidskql6671@naver.com>
2019-12-16 19:46:49 +0900

    버그 수정

--- UserAccount/Student.py
+++ UserAccount/Student.py
@@ -1,19 +1,24 @@
-from StudentDB import StudentDB
+from DBConnection.StudentDB import *
 from UserAccount.UserAccount import UserAccount
 
-class Student(UserAccount):
 
+class Student(UserAccount):
     def __init__(self):
+        super().__init__()
+
         self._user_DB = StudentDB()
         self._user_info = None
+
+        self._user_id = ""
+        self._user_pw = ""
 
     def get_info(self):
         return self._user_info
 
-    def login( self, input_id, input_pwd ):
+    def login(self, input_id, input_pwd):
 
         self._user_info = self._user_DB.search((input_id, input_pwd))
-        if None:
+        if not self._user_info:
             return False
         else:
             return True
@@ -21,11 +26,5 @@ class Student(UserAccount):
     def register_acc(self, user_id, user_pw, student_id, name, major, is_abeek):
         self._user_DB.register((user_id, user_pw, student_id, name, major, is_abeek))
 
-    def del_acc(self, user_id, user_pw):
+    def del_acc(self):
         self._user_DB.delete((self._user_id, self._user_pw))
-
-    def get_abeek(self):
-        return self._is_ABEEK
-
-    def get_major(self):
-        return self._major
