4b360897a5755061985b5876b937cf0289744c8b
gidskql6671 <gidskql6671@naver.com>
2019-12-18 03:27:13 +0900

    asd

--- GraduationRequirements/GradRequirement.py
+++ GraduationRequirements/GradRequirement.py
@@ -7,16 +7,3 @@ class GradRequirement:
         self._gr_list = []
 
 
-    #디비에 해당 학과의 졸업 요건 추가
-    def new_requirement(self):
-        GradRequirementsDB.new_requirement(self.major,self.sub_major,self.field,self.field_name,self.value,self.description)
-
-    #디비에 해당 학과의 졸업요건 변경
-    def edit_requirement(self):
-        GradRequirementsDB.edit_requirement(self.major,self.sub_major,self.field,self.field_name,self.value,self.description)
-
-    #디비에 해당 학과의 졸업요건 삭제
-    def del_requirment(self):
-        GradRequirementsDB.delete_requirement(self.major,self.sub_major,self.field)
-
-
